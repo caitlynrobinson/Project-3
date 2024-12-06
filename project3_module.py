e6bd631b0892438ba1ac44cb48bde770b3393572
caitlynrobinson <crobin13@uvm.edu>
2024-12-05 20:51:49 -0500

    Update project3_module.py

--- project3_module.py
+++ project3_module.py
@@ -13,42 +13,8 @@ DESCRIPTION
 import numpy as np 
 import matplotlib.pyplot as plt
 
-#%% Part 1: Collect and Load Data
-def load_data(input_files):
-    """
-    This function works to load the four datasets collected with the Arduino.
-
-    Parameters
-    ----------
-    input_files : list of strings
-        The paths of each of the four input files which correspond to Arduino activity datasets.
-
-    Returns
-    -------
-    contents : list of strings
-        The data that corresponds to each Ardunio dataset, separated by row.
-
-    """
-    contents = [open(path).read() for path in input_files]
-    return contents
-
-
-def separate_datasets(contents):
-    """
-    This function separates each dataset that was loaded with load_data, so each activity corresponds to its own list
-
-    Parameters
-    ----------
-    contents : list of strings
-        The data that corresponds to each Ardunio dataset, separated by row.
-
-    Returns
-    -------
-    list
-        DESCRIPTION.
-
-    """
-    return [content.split('\n') for content in contents]
+#def plot_raw_data(rest, relax, mental_stress, wall_sit):
+ #   for 
 
 #%% Part 2: Filter Your Data
 
@@ -57,20 +23,3 @@ def separate_datasets(contents):
 #%% Part 4: Calculate Heart Rate Variability
 
 #%% Part 5: Get HRV Frequency Band Power
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
